66448e3bd89b0d2b7745140d627e7b10787fe323
Harry <wang.haoda@protonmail.com>
2017-09-27 18:54:33 -0400

    add nativebase to the thing

--- app.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa');
-const app = module.exports = new Koa();
-
-app.use(async function(ctx) {
-    ctx.body = 'Welcome to Bustle!';
-});
-
-if (!module.parent) app.listen(8080);
