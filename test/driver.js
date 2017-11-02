5c530b4418dddb85fe72cfecf0092b376198bbac
Harry <wang.haoda@protonmail.com>
2017-11-02 18:17:36 -0400

    add tests for driver APIs

---
+++ test/driver.js
@@ -0,0 +1,33 @@
+const app = require('../app');
+const request = require('supertest').agent(app.listen());
+
+describe('Driver Functions', function() {
+    it('Driver sign up success', function(done) {
+        request
+            .post('/driver/signup/')
+            .field('username', 'bobross')
+            .field('password', 'paintingmagic')
+            .field('name', 'Bob Ross')
+            .field('school', '1')
+            .expect(200)
+            .end(function(err, res) {
+                if (err)
+                    return done(err);
+                done();
+            });
+    });
+    it('driver set location success', function(done) {
+        request
+            .get('/driver/update_location')
+            .field('id', '1')
+            .field('lat', '3')
+            .field('long', '3')
+            .expect(200)
+            .end(function(err, res) {
+                if (err)
+                    return done(err);
+                done();
+            });
+    });
+});
+
