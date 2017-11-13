853affe37ed1c67a36a3e82a02d89b9b0fa29d96
veggiedefender <jessejesse123@gmail.com>
2017-11-13 16:54:44 -0500

    split up .use() statements and sync outside of a promise

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const Koa = require('koa');
 const router = require('./route');
 const bodyParser = require('koa-bodyparser');
-const { sequelize, Sequelize } = require('./db/postgres');
+const { sequelize } = require('./db/postgres');
 
 const app = new Koa();
 
-app
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .use((ctx) => {
-    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
-  });
-
-sequelize.sync().then(() => {
-  app.listen(8080);
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use((ctx) => {
+  ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
 });
+
+sequelize.sync();
+app.listen(8080);
 
 module.exports = app;
