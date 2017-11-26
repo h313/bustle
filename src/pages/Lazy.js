7df1443143a2c59ded34142e04338c74b1e0b387
veggiedefender <jessejesse123@gmail.com>
2017-11-26 12:12:15 -0500

    add react router stuff

---
+++ src/pages/Lazy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default () => (
+  <div>
+    <Link to="/superLazy">super lazy</Link>
+    <p>this was lazily loaded</p>
+  </div>
+)
---
+++ src/pages/SuperLazy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default () => (
+  <div>
+    <Link to="/">lazy</Link>
+    <p>super lazy!</p>
+  </div>
+)
