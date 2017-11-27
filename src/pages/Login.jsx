f0bca0c3896f4b89ead25d3475d8fdc6db76bbd4
Errant9 <eruebush@gmail.com>
2017-11-26 20:38:00 -0500

    Added username and password fields and a login button to Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { Button, Form, Input } from 'semantic-ui-react';
 export default () => (
   <div>
   <div className='bustleHeader'>
@@ -13,5 +13,20 @@ export default () => (
       </Link>
     </div>
     <h1>Login</h1>
+    <Form>
+      <Form.Field
+        control={Input}
+        label='Username'
+        placeholder='Username'
+      />
+
+      <Form.Field
+        control={Input}
+        label='Password'
+        placeholder='Password'
+      />
+    </Form>
+    <p/>
+    <Button>Login</Button>
   </div>
 );
