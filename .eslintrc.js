ee599a5a1024a7b8edd7fcaefafe56061d929ebc
Zhenphos <Jonathan.Zhang111@gmail.com>
2018-01-22 07:30:32 -0500

    update code style to conform to latest ES6 standards

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
-module.exports = {
-  "extends": "airbnb",
-  "env": {
-    "browser": true,
-    "node": true
-  },
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "sourceType": "module",
-    "allowImportExportEverywhere": false,
-    "codeFrame": false
-  }
-};
+module.exports = { 
+  "extends": "airbnb", 
+  "env": { 
+    "browser": true, 
+    "node": true 
+  }, 
+  "parser": "babel-eslint", 
+  "parserOptions": { 
+    "sourceType": "module", 
+    "allowImportExportEverywhere": false, 
+    "codeFrame": false 
+  } 
+}; 
